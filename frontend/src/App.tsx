6a833c348974284d272e159d8f7d04444d7ae750
RafaelSoaresSMP <rafael.soares@simplesmeiosdepagamento.com.br>
2024-04-03 16:57:06 -0300

    feat: configure docker volume to update front in real time

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -25,7 +25,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React </h1>
+      <h1>Vite + Reactff fsafds dfsfds </h1>
       <div className="card">
         <button onClick={()=>requestAPI()}>
           count ée {count}
